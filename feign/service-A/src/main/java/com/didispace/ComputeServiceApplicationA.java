febb797c1f2abd685936f330018bc3b23fa29792
wupeixu <wupeixu@lexue.com>
2018-08-09 18:09:27 +0800

    feign

---
+++ feign/service-A/src/main/java/com/didispace/ComputeServiceApplicationA.java
@@ -0,0 +1,18 @@
+package com.didispace;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+/**
+ * 服务service-A
+ * @author wpx
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ComputeServiceApplicationA {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ComputeServiceApplicationA.class).web(true).run(args);
+	}
+
+}
