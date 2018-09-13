c5f410b2cbb9228834a0f925f97e7eb535099c87
wupeixu <wupeixu@lexue.com>
2018-09-13 20:02:41 +0800

    qq

--- feign/service-A/src/main/java/com/didispace/ComputeServiceApplicationA.java
+++ feign/service-A/src/main/java/com/didispace/ComputeServiceApplicationA.java
@@ -14,5 +14,5 @@ public class ComputeServiceApplicationA {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ComputeServiceApplicationA.class).web(true).run(args);
 	}
-
+  
 }
